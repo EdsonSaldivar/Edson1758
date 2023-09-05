f1fd5386cef11379dbe2984dbf849a387b5dd48b
EdsonSaldivar <137822795+EdsonSaldivar@users.noreply.github.com>
2023-09-05 16:16:49 -0600

    Add files via upload

---
+++ Clase 05-09-2023/app3.js
@@ -0,0 +1,28 @@
+let suma= 256 + 256;
+console.log("256 + 256 = " + suma);
+
+let resta= 512 - 256;
+console.log("512 - 256 = " + resta);
+
+let multiplicacion= 256 * 256;
+console.log("256 * 256 = " + multiplicacion);
+
+let division= 256 / 256;
+console.log("256 / 256 = " + division);
+
+//app4.js Empieza aqui:
+
+let nombreUser = "EdsonRS";
+let edad = 21;
+let nacionalidad = "Mexicano";
+let comprometido = "Zy";
+
+
+console.log("Nombre es:");
+console.log(nombreUser);
+console.log("Edad es:");
+console.log(edad);
+console.log("Su nacionalidad es:");
+console.log(nacionalidad);
+console.log("Estas comprometido?");
+console.log(comprometido);
